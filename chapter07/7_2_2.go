9ae0955322c2d13ee10c0dd4b9b8c6b1c0fb5acd
rightgo09 <skyarrow09@gmail.com>
2017-12-12 07:18:34 +0900

    chapter07

---
+++ chapter07/7_2_2.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net"
+	"fmt"
+)
+
+func main() {
+	conn, err := net.Dial("udp4", "localhost:8080")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	fmt.Println("Sending to server")
+	_, err = conn.Write([]byte("Hello from client"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Receiving from server")
+	buffer := make([]byte, 1500)
+	length, err := conn.Read(buffer)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Received: %s\n", string(buffer[:length]))
+}
